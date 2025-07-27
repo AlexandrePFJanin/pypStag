1000eb19716c3128535bfa8b114a61eb820ae6dd
Alexandre Janin <alexandre.janin7@gmail.com>
2025-07-27 21:28:06 +0200

    setup update PEP 517
    Updated installation configuration for pip and PEP 517 compliance

--- setup.py
+++ setup.py
@@ -1,22 +1,31 @@
 from setuptools import setup
 
 setup(
-   name='pypStag',
-   version='1.0.0',
-   author='Alexandre JANIN',
-   author_email='alexandre.janin@protonmail.com',
-   url='https://github.com/AlexandrePFJanin/pypStag',
-   packages=['pypStag'],
-   data_files=[('pypStag', ['pypStag/fields/stagyy-fields-defaults', 'pypStag/fields/stagyy-fields-local'])],     # to include data samples
-   include_package_data=True,   # to include data samples
-   license='LICENSE.md',
-   description='A python package for post-processing and analysing StagYY outputs.',
-   long_description=open('README.md').read(),
-   install_requires=[
+    name='pypStag',
+    version='1.0.1',
+    author='Alexandre JANIN',
+    author_email='alexandre.janin@protonmail.com',
+    url='https://github.com/AlexandrePFJanin/pypStag',
+    description='A python package for post-processing and analysing StagYY outputs.',
+    long_description=open('README.md', encoding='utf-8').read(),
+    long_description_content_type='text/markdown',
+    license='LICENSE.md',
+    packages=['pypStag'],
+    include_package_data=True,
+    package_data={
+        'pypStag': ['fields/*'],
+    },
+    install_requires=[
         'ipython>=8.15.0',
         'numpy>=1.12',
-	    'scipy>=1.5.2',
-	    'h5py>=3.9.0',
-	    'termcolor'
-   ],
+        'scipy>=1.5.2',
+        'h5py>=3.9.0',
+        'termcolor',
+    ],
+    classifiers=[
+        'Programming Language :: Python :: 3',
+        'License :: OSI Approved :: Apache Software License',
+        'Operating System :: OS Independent',
+    ],
+    python_requires='>=3.7',
 )
